33d2bc8eec6ac04673d33c48cceee264641ce90b
leeraiyan <42810002+leeraiyan@users.noreply.github.com>
2022-03-01 14:08:37 +0800

    update fully functional code

--- ArduinoPython/tryingtrying.py
+++ ArduinoPython/tryingtrying.py
@@ -1,5 +1,6 @@
 from bluepy import btle
 import time
+from datetime import datetime
 
 class MyDelegate(btle.DefaultDelegate):
     def __init__(self):
@@ -16,8 +17,12 @@ print("Connected to Bluno")
 p.setDelegate( MyDelegate() )
 # Setup to turn notifications on, e.g.
 svc = p.getServiceByUUID("0000dfb0-0000-1000-8000-00805f9b34fb")
+counter = 0
+starttime = datetime.now()
 
-while True:
+while (counter < 600):
+    counter += 1
     if p.waitForNotifications(0):
-        #print("Notified!")
         continue
+        
+print("took", datetime.now() - starttime)
